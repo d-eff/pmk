7f29cd12bb420640cf10cc5d7912e5665e7f6b96
David Fernandez <dfernandez06@gmail.com>
2018-07-21 22:55:07 -0400

    alright, we're actually talking to the db

--- server/db/schemas/world.js
+++ server/db/schemas/world.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 
-const worldSchema = mongoose.Schema({
-  name: String
-});
+const worldSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    collection: 'worlds',
+  },
+);
 
 module.exports = mongoose.model('world', worldSchema);
--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 require('./db/db.js');
+const world = require('./db/schemas/world.js');
 
-app.get('/', (req, res) => res.send('sup'));
+app.get('/', (req, res) => {
+  world.find((err, results) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.json(results);
+    }
+  });
+});
 
 app.listen(3000, () => {
   console.log('we running');
