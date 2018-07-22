6557768abde2c57bce9ca241dda83e0513ce3317
David Fernandez <dfernandez06@gmail.com>
2018-07-21 21:49:45 -0400

    Tetsuo is being terrorized by giant toys

--- server/server.js
+++ server/server.js
@@ -1,2 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const app = express();
+
+app.get('/', (req, res) => res.send('sup') );
+
+app.listen(3000, () => console.log('we running') );
