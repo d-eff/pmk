12750c1b69f19f29bc00fbf8bfeb11dc8dc6d3b6
David Fernandez <dfernandez06@gmail.com>
2018-07-28 15:55:12 -0400

    El Camino on Dayton's

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,16 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const config = require('../.config/dev.config.js');
 
+router.post('/signup', passport.authenticate('signup', { session: false }), async (req, res, next) => {
+  req.login(req.user, { session: false }, async (error) => {
+    if (error) return next(error);
+    const body = { _id: req.user.id, username: req.user.username };
+    const token = jwt.sign({ user: body }, config.jwtSecret, { expiresIn: '1d' });
+    return res.json({ token });
+  });
+});
+
+
 router.post('/login', async (req, res, next) => {
   passport.authenticate('login', async (err, user) => {
     try {
